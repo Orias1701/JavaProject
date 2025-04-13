b2f11283f2c192bf1b08751e5869ca19ab523701
Orias1701 <long171.d.shine@gmail.com>
2025-04-13 19:05:29 +0700

    2

--- src/Run.java
+++ src/Run.java
@@ -4,6 +4,7 @@ import view.MainUI;
 
 public class Run {
     public static void main(String[] args) {
+        System.out.println("Starting the application...");
         SwingUtilities.invokeLater(() -> {
             LogHandler.logInfo("Start");
             new MainUI();
